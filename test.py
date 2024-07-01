7d3ff899ea63ab0fffa977a7c3af009d55be1ca6
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-01 16:37:02 +0200

    Changes to be committed: 	modified:   test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 # Import python packages
-import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
+import streamlit as st # type: ignore
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+import requests # type: ignore
 import json
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col, when_matched # type: ignore
 
 df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
 
