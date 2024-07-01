1a6ae4fbac2980cc90696370981e6926517bd3a8
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-01 14:04:05 +0200

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -7,9 +7,7 @@ from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 from snowflake.snowpark.functions import col
 
