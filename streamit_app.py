f0b7225522c395fbfe57651b56f742d6e0272910
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2024-07-02 09:58:48 +0200

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -27,6 +27,8 @@ pd_df = my_dataframe.to_pandas()
 name_on_order = st.text_input("Name on Smoothie:")
 #st.write("The name on your Smoothie will be:", name_on_order)
 st.info("The name on your Smoothie will be: " + name_on_order)
+if name_on_order:
+    st.balloons()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
